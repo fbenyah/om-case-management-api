c8f8fae26518dcdbb523495e1097676f3b9649a0
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 09:35:44 +0200

    refac: test models

---
+++ tests/om.servicing.casemanagement.tests/Shared/Models/DummyCustomException.cs
@@ -0,0 +1,13 @@
+﻿using OM.RequestFramework.Core.Exceptions;
+
+namespace om.servicing.casemanagement.tests.Shared.Models;
+
+public class DummyCustomException : ICustomException
+{
+    int ICustomException.HttpResponseCode => throw new NotImplementedException();
+
+    IErrorResponse ICustomException.ToErrorResponse()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/om.servicing.casemanagement.tests/Shared/Models/TestFluentValidationError.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation.Results;
+using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.tests.Shared.Models;
+
+public class TestFluentValidationError : BaseFluentValidationError
+{
+    public TestFluentValidationError() : base() { }
+    public TestFluentValidationError(ValidationResult result) : base(result) { }
+}
