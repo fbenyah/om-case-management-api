20e8b8fa07a13066521351bbafe0fdb819d5248a
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 14:16:13 +0200

    refac: om case service

---
+++ src/om.servicing.casemanagement.application/Services/IOMCaseService.cs
@@ -0,0 +1,9 @@
+﻿using om.servicing.casemanagement.domain.Dtos;
+
+namespace om.servicing.casemanagement.application.Services;
+
+public interface IOMCaseService
+{
+    Task<List<OMCaseDto>> GetCasesForCustomer(string identityNumber);
+    Task<List<OMCaseDto>> GetCasesForCustomerByStatusAsync(string identityNumber, string status);
+}
---
+++ src/om.servicing.casemanagement.application/Services/OMCaseService.cs
@@ -0,0 +1,58 @@
+﻿using om.servicing.casemanagement.data.Repositories.Shared;
+using om.servicing.casemanagement.domain.Dtos;
+using om.servicing.casemanagement.domain.Entities;
+using om.servicing.casemanagement.domain.Mappings;
+
+namespace om.servicing.casemanagement.application.Services;
+
+public class OMCaseService : IOMCaseService
+{
+    private readonly IGenericRepository<OMCase> _caseRepository;
+
+    public OMCaseService(IGenericRepository<OMCase> caseRepository)
+    {
+        _caseRepository = caseRepository;
+    }
+
+    public async Task<List<OMCaseDto>> GetCasesForCustomer(string identityNumber)
+    {
+        if (string.IsNullOrWhiteSpace(identityNumber))
+        {
+            return new List<OMCaseDto>();
+        }
+
+        IEnumerable<OMCase> omCases = await _caseRepository.FindAsync(c => c.IdentificationNumber == identityNumber);
+
+        return ReturnCaseListDto(omCases);
+    }
+
+    public async Task<List<OMCaseDto>> GetCasesForCustomerByStatusAsync(string identityNumber, string status)
+    {
+        if (string.IsNullOrWhiteSpace(identityNumber)
+            || string.IsNullOrWhiteSpace(status))
+        {
+            return new List<OMCaseDto>();
+        }
+
+        IEnumerable<OMCase> omCases = await _caseRepository.FindAsync(c => c.Status == status && c.IdentificationNumber == identityNumber);
+
+        return ReturnCaseListDto(omCases);
+    }
+
+    private List<OMCaseDto> ReturnCaseListDto(IEnumerable<OMCase> omCases)
+    {
+        List<OMCaseDto> caseListDto = new List<OMCaseDto>();
+
+        if (omCases == null || !omCases.Any())
+        {
+            return caseListDto;
+        }
+
+        foreach (OMCase omCase in omCases)
+        {
+            caseListDto.Add(EntityToDtoMapper.ToDto(omCase));
+        }
+
+        return caseListDto;
+    }
+}
