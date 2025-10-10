4838fb485afdbe5bfe108792df52bd5a36266540
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 08:47:39 +0200

    refac: client exceptions

---
+++ src/om.servicing.casemanagement.domain/Exceptions/Client/ConflictException.cs
@@ -0,0 +1,13 @@
+﻿using OM.RequestFramework.Core.Exceptions;
+using static OM.RequestFramework.Core.Exceptions.ExceptionConstants;
+
+namespace om.servicing.casemanagement.domain.Exceptions.Client;
+
+public class ConflictException : ClientException
+{
+    public override int HttpResponseCode => 409;
+
+    public ConflictException(string message = ErrorMessages.ValidationError) : base(message)
+    {
+    }
+}
---
+++ src/om.servicing.casemanagement.domain/Exceptions/Client/TooManyRequestsException.cs
@@ -0,0 +1,13 @@
+﻿using OM.RequestFramework.Core.Exceptions;
+using static OM.RequestFramework.Core.Exceptions.ExceptionConstants;
+
+namespace om.servicing.casemanagement.domain.Exceptions.Client;
+
+public class TooManyRequestsException : ClientException
+{
+    public override int HttpResponseCode => 429;
+
+    public TooManyRequestsException(string message = ErrorMessages.ValidationError) : base(message)
+    {
+    }
+}
