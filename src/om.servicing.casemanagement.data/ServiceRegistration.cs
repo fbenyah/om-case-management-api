109d6ec5ee99a6aa3d4ac925c516738444fd2c17
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 14:14:48 +0200

    refac: register generic repository implementations

--- src/om.servicing.casemanagement.data/ServiceRegistration.cs
+++ src/om.servicing.casemanagement.data/ServiceRegistration.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using om.servicing.casemanagement.data.Context;
+using om.servicing.casemanagement.data.Repositories.Shared;
+using om.servicing.casemanagement.domain.Entities;
 
 namespace om.servicing.casemanagement.data;
 
@@ -24,6 +26,19 @@ public static class ServiceRegistration
         {
             options.UseNpgsql(configuration.GetConnectionString("CaseManagementDatabasePostgres"));
         });
+
+        // Register the generic repository implementations
+        RegisterRepositories<OMCase>(services);
+        RegisterRepositories<OMInteraction>(services);
+        RegisterRepositories<OMTransaction>(services);
+        RegisterRepositories<OMTransactionType>(services);
+
         return services;
+    }
+
+    private static void RegisterRepositories<T>(this IServiceCollection services) where T : class
+    {
+        services.AddScoped<IGenericRepository<T>>(provider =>
+                new GenericRepository<T, CaseManagerContext>(provider.GetRequiredService<CaseManagerContext>()));
     }
 }
