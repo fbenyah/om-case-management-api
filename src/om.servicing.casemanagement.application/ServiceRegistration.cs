d6ac329ac9d4e0af6b78b9f02a544d13e736a181
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 14:15:26 +0200

    refac: register omcase service

---
+++ src/om.servicing.casemanagement.application/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using om.servicing.casemanagement.application.Services;
+
+namespace om.servicing.casemanagement.application;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection RegisterCustomServices(this IServiceCollection services)
+    {
+        // Register application services here
+        services.AddScoped<IOMCaseService, OMCaseService>();
+
+        return services;
+    }
+}
