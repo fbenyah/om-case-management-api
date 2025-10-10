7fbac44140cbab5690c3adf986b37019e42b1930
Francis Benyah <francis.benyah@gmail.com>
2025-10-10 14:15:05 +0200

    refac: include identification number

--- src/om.servicing.casemanagement.domain/Entities/OMCase.cs
+++ src/om.servicing.casemanagement.domain/Entities/OMCase.cs
@@ -10,11 +10,15 @@ namespace om.servicing.casemanagement.domain.Entities;
 /// through which the case was created and a collection of related interactions.</remarks>
 [Table("case")]
 public class OMCase : BaseEntityWithStatus
-{    
+{
     [Required]
     [MaxLength(70)]
     [Column("channel", Order = 2)]
     public string Channel { get; set; }
+
+    [MaxLength(20)]
+    [Column("identificationNumber", Order = 3)]
+    public string IdentificationNumber { get; set; }
 
     public ICollection<OMInteraction> Interactions { get; set; }
 }
