a52695bbe259d868b86d7af1536f15597b96cf95
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 15:05:00 +0200

    refac: mediatr behaviour and exception logging

---
+++ src/om.servicing.casemanagement.application/Features/Configuration/MediatorExceptionLoggingHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using OM.RequestFramework.Core.Logging;
+
+namespace om.servicing.casemanagement.application.Features.Configuration;
+
+/// <summary>
+/// Handles exceptions that occur during the processing of a request by logging the exception details.
+/// </summary>
+/// <remarks>This handler logs the exception details, including any inner exceptions, using the provided logging
+/// service. It does not modify the state of the request or response.</remarks>
+/// <typeparam name="TRequest">The type of the request being processed.</typeparam>
+/// <typeparam name="TResponse">The type of the response expected from the request.</typeparam>
+/// <typeparam name="TException">The type of the exception being handled. Must derive from <see cref="Exception"/>.</typeparam>
+public class MediatorExceptionLoggingHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
+        where TRequest : IRequest<TResponse>
+        where TException : Exception
+{
+    private readonly ILoggingService _loggingService;
+
+    public MediatorExceptionLoggingHandler(ILoggingService loggingService)
+    {
+        _loggingService = loggingService;
+    }
+
+    public Task Handle(TRequest request, TException ex, RequestExceptionHandlerState<TResponse> state, CancellationToken cancellationToken)
+    {
+        _loggingService.LogError($"Something went wrong while handling request of type {typeof(TRequest)}", ex);
+
+        Exception exception = ex;
+        while (exception.InnerException != null)
+        {
+            exception = exception.InnerException;
+            _loggingService.LogError(exception.Message, exception);
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/om.servicing.casemanagement.application/Features/Configuration/MediatorLoggingBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.Extensions.Options;
+using om.servicing.casemanagement.domain.Configuration.Options;
+using OM.RequestFramework.Core.Enums;
+using OM.RequestFramework.Core.Logging;
+using System.Diagnostics;
+
+namespace om.servicing.casemanagement.application.Features.Configuration;
+
+/// <summary>
+/// Provides logging behavior for MediatR request pipelines, logging details about the request and its execution time.
+/// </summary>
+/// <remarks>This behavior logs the start and end of the request handling process, including the execution time.
+/// Additional logging is performed in non-production environments, including detailed request information.</remarks>
+/// <typeparam name="TRequest">The type of the request being handled.</typeparam>
+/// <typeparam name="TResponse">The type of the response returned by the request handler.</typeparam>
+public class MediatorLoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private ILoggingService _loggingService;
+    private readonly IOptions<EnvironmentOptions> _environmentOptions;
+
+    /// <summary>
+    /// Create an instance using dependency injections
+    /// </summary>
+    /// <param name="loggingService"></param>
+    /// <param name="environmentOptions"></param>
+    public MediatorLoggingBehaviour(ILoggingService loggingService, IOptions<EnvironmentOptions> environmentOptions)
+    {
+        _loggingService = loggingService;
+        _environmentOptions = environmentOptions;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestGuid = Guid.NewGuid().ToString();
+        var requestName = typeof(TRequest).Name;
+        var stopWatch = Stopwatch.StartNew();
+        TResponse response;
+
+        // Request 
+        _loggingService.LogInfo($"Handle {requestName} [{requestGuid}]");
+
+        if (_environmentOptions.Value.IsNonProd)
+        {
+            _loggingService.LogInfoInMethod(className: "", requestName, request, $"Handling {requestName} [{requestGuid}]", LogDirection.Incoming);
+        }
+
+        try
+        {
+            // Action Request Handler
+            response = await next();
+        }
+        finally
+        {
+            stopWatch.Stop();
+            _loggingService.LogInfo($"Handled {requestName} [{requestGuid}]; Execution time: {stopWatch.ElapsedMilliseconds}ms");
+        }
+
+        // Response
+        return response;
+    }
+}
