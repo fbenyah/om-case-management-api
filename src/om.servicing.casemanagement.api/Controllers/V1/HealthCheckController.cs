0181e442c0d3962f511105ca2a1bbc6c15cbb974
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 16:23:35 +0200

    refac: add a health check

---
+++ src/om.servicing.casemanagement.api/Controllers/V1/HealthCheckController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace om.servicing.casemanagement.api.Controllers.V1;
+
+[ApiController]
+[Route("/api/casemanagement/v{version:apiVersion}/[controller]")]
+public class HealthCheckController : BaseController
+{
+    [SwaggerOperation(
+        Summary = "Health check endpoint.",
+        Description = @"This request is to be used for the health check for this api.")]
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status502BadGateway)]
+    [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
+    public IActionResult Get()
+    {
+        string message = $"CASE MANAGEMENT API is running - {DateTime.Now.ToString()}";
+
+        LoggingService.LogInfo(message);
+        return Ok(message);
+    }
+}
