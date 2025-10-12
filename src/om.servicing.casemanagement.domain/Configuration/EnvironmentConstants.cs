f8c3f92df7b5870e5e5f260117ff6f25c8b15edd
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 15:05:24 +0200

    refac: initial environment considerations

---
+++ src/om.servicing.casemanagement.domain/Configuration/EnvironmentConstants.cs
@@ -0,0 +1,32 @@
+﻿namespace om.servicing.casemanagement.domain.Configuration;
+
+/// <summary>
+/// Provides a set of predefined environment names commonly used in application lifecycle management.
+/// </summary>
+/// <remarks>The <see cref="Environments"/> class defines constants representing typical application environments,
+/// such as <see cref="Development"/>, <see cref="Staging"/>, and <see cref="Production"/>. These values can be used to
+/// configure or identify the current runtime environment of an application.</remarks>
+public static class Environments
+{
+    public const string Development = "Development";
+    public const string Staging = "Staging";
+    public const string Production = "Production";
+}
+
+/// <summary>
+/// Provides constants representing common deployment environments.
+/// </summary>
+/// <remarks>This class defines string constants for standard deployment environments, such as  development,
+/// quality assurance, and production. These constants can be used to  standardize environment identifiers across
+/// applications.</remarks>
+public static class DeploymentEnvironments
+{
+    public const string Development = "dev";
+    public const string QualityAssurance = "qa";
+    public const string Production = "prod";
+}
+
+public static class Logging
+{
+    public const string LoggingVerbosityLevel = "LOGGING_VERBOSITY_LEVEL";
+}
---
+++ src/om.servicing.casemanagement.domain/Configuration/Options/EnvironmentOptions.cs
@@ -0,0 +1,43 @@
+﻿namespace om.servicing.casemanagement.domain.Configuration.Options;
+
+/// <summary>
+/// Represents configuration options for managing application environments, including environment name, variant,
+/// deployment environment, and production status.
+/// </summary>
+/// <remarks>This class provides properties to configure and retrieve details about the application's current
+/// environment. The <see cref="EnvironmentName"/> property determines the environment type (e.g., Development, Staging,
+/// Production) and updates related properties such as <see cref="DeploymentEnvironment"/> and <see cref="IsNonProd"/>
+/// accordingly.</remarks>
+public class EnvironmentOptions
+{
+    private string _environmentName = string.Empty;
+    public string EnvironmentName { get { return _environmentName; } set { SetEnvironmentNameAndUpdateOptions(value); } }
+    public string? EnvironmentVariant { get; set; }
+    public string DeploymentEnvironment { get; private set; } = string.Empty;
+
+    public bool IsNonProd { get; private set; }
+
+    private void SetEnvironmentNameAndUpdateOptions(string environmentName)
+    {
+        _environmentName = environmentName;
+
+        IsNonProd = Environments.Development.Equals(environmentName, StringComparison.InvariantCultureIgnoreCase) ||
+            Environments.Staging.Equals(environmentName, StringComparison.InvariantCultureIgnoreCase);
+
+        switch (environmentName)
+        {
+            case Environments.Development:
+                DeploymentEnvironment = DeploymentEnvironments.Development;
+                break;
+            case Environments.Staging:
+                DeploymentEnvironment = DeploymentEnvironments.QualityAssurance;
+                break;
+            case Environments.Production:
+                DeploymentEnvironment = DeploymentEnvironments.Production;
+                break;
+            default:
+                DeploymentEnvironment = string.Empty;
+                break;
+        }
+    }
+}
