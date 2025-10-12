14ab4c8fadbd0ae5d9e6878002a70e3d5fd49316
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 18:11:21 +0200

    refac: add constants

---
+++ src/om.servicing.casemanagement.domain/Constants/CaseManagementConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace om.servicing.casemanagement.domain.Constants;
+
+public class CaseManagementConstants
+{
+    public static class HttpHeaders
+    {
+        public const string XSourceSystem = "X-Source-System";
+    }
+}
