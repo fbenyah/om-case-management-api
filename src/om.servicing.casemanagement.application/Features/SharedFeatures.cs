945ff2d77551fc33303cab9b4d25a497a12b2644
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 16:43:43 +0200

    refac: change access modifier

--- src/om.servicing.casemanagement.application/Features/SharedFeatures.cs
+++ src/om.servicing.casemanagement.application/Features/SharedFeatures.cs
@@ -4,7 +4,7 @@ namespace om.servicing.casemanagement.application.Features;
 
 public abstract class SharedFeatures
 {
-    internal readonly ILoggingService _loggingService;
+    protected internal readonly ILoggingService _loggingService;
 
     public SharedFeatures(
             ILoggingService loggingService
