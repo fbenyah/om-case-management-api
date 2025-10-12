d5caf31973d537e1097440eafb7113025b8c397a
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 16:43:56 +0200

    refac: add tests for shared features

---
+++ tests/om.servicing.casemanagement.tests/Application/Features/SharedFeaturesTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using om.servicing.casemanagement.application.Features;
+using OM.RequestFramework.Core.Logging;
+
+namespace om.servicing.casemanagement.tests.Application.Features;
+
+public class SharedFeaturesTests
+{
+    [Fact]
+    public void Constructor_SetsLoggingService()
+    {
+        var loggingServiceMock = new Mock<ILoggingService>();
+        var testFeature = new TestSharedFeatures(loggingServiceMock.Object);
+
+        Assert.NotNull(testFeature.GetLoggingService());
+        Assert.Equal(loggingServiceMock.Object, testFeature.GetLoggingService());
+    }
+}
+
+public class TestSharedFeatures : SharedFeatures
+{
+    public TestSharedFeatures(ILoggingService loggingService) : base(loggingService) { }
+    public ILoggingService GetLoggingService() => _loggingService;
+}
