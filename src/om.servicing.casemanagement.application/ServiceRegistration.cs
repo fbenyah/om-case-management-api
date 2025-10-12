8576bad111c6664c57f6403ebce1b7eeb724f7f1
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 15:04:27 +0200

    refac: register mediatr and behavior

--- src/om.servicing.casemanagement.application/ServiceRegistration.cs
+++ src/om.servicing.casemanagement.application/ServiceRegistration.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using om.servicing.casemanagement.application.Features.Configuration;
 using om.servicing.casemanagement.application.Services;
 
 namespace om.servicing.casemanagement.application;
 
 public static class ServiceRegistration
 {
+    public static IServiceCollection RegisterApplicationFeatures(this IServiceCollection services)
+    {
+        return services
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(MediatorLoggingBehaviour<,>))
+                .AddTransient(typeof(IRequestExceptionHandler<,,>), typeof(MediatorExceptionLoggingHandler<,,>));
+    }
+
     public static IServiceCollection RegisterCustomServices(this IServiceCollection services)
     {
         // Register application services here
