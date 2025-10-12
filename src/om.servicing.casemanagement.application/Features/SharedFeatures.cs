e6e93e6e53c71e9ffcdb1e17699598c3e0006373
Francis Benyah <francis.benyah@gmail.com>
2025-10-12 15:33:45 +0200

    refac: include a shared feature

---
+++ src/om.servicing.casemanagement.application/Features/SharedFeatures.cs
@@ -0,0 +1,15 @@
+﻿using OM.RequestFramework.Core.Logging;
+
+namespace om.servicing.casemanagement.application.Features;
+
+public abstract class SharedFeatures
+{
+    internal readonly ILoggingService _loggingService;
+
+    public SharedFeatures(
+            ILoggingService loggingService
+        )
+    {
+        _loggingService = loggingService;
+    }
+}
