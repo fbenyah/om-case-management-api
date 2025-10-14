331b0fb723893ba003f021bc17113f40e4e19c78
Francis Benyah <francis.benyah@gmail.com>
2025-10-14 19:30:33 +0200

    refac: tests for operational business segments

---
+++ tests/om.servicing.casemanagement.tests/Domain/Enums/OperationalBusinessSegmentTests.cs
@@ -0,0 +1,27 @@
+﻿using om.servicing.casemanagement.domain.Enums;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace om.servicing.casemanagement.tests.Domain.Enums;
+
+public class OperationalBusinessSegmentTests
+{
+    [Fact]
+    public void Enum_HasExpectedValues()
+    {
+        Assert.Equal(1, (int)OperationalBusinessSegment.Unknown);
+        Assert.Equal(2, (int)OperationalBusinessSegment.CustomerServicing);
+    }
+
+    [Theory]
+    [InlineData(OperationalBusinessSegment.Unknown, "Unknown")]
+    [InlineData(OperationalBusinessSegment.CustomerServicing, "Customer Servicing")]
+    public void Enum_HasExpectedDescriptionAttribute(OperationalBusinessSegment segment, string expectedDescription)
+    {
+        var type = typeof(OperationalBusinessSegment);
+        var memberInfo = type.GetMember(segment.ToString()).First();
+        var descriptionAttribute = memberInfo.GetCustomAttribute<DescriptionAttribute>();
+        Assert.NotNull(descriptionAttribute);
+        Assert.Equal(expectedDescription, descriptionAttribute.Description);
+    }
+}
