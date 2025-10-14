1f1012da264e1b9153ff2c2f4014ab3144dadc79
Francis Benyah <francis.benyah@gmail.com>
2025-10-14 19:30:21 +0200

    refac: enum for operational business segment

---
+++ src/om.servicing.casemanagement.domain/Enums/OperationalBusinessSegment.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace om.servicing.casemanagement.domain.Enums;
+
+public enum OperationalBusinessSegment
+{
+    [Description("Unknown")]
+    Unknown = 1,
+
+    [Description("Customer Servicing")]
+    CustomerServicing
+}
