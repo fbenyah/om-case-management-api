fd82c3addfafc4056f6aad46a817dd28d6cb0497
Francis Benyah <francis.benyah@gmail.com>
2025-10-14 19:48:10 +0200

    refac: add base service response

---
+++ src/om.servicing.casemanagement.domain/Responses/Shared/ServiceBaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace om.servicing.casemanagement.domain.Responses.Shared;
+
+public abstract class ServiceBaseResponse<T> : BaseFluentValidationError
+{
+    public DateTime ResponseTime { get; private set; } = DateTime.Now;
+
+    [JsonPropertyName("data")]
+    public T Data { get; set; }
+}
