f0f07cd235577d63f169d9b428c78fd1af567121
Francis Benyah <francis.benyah@gmail.com>
2025-10-14 19:47:17 +0200

    refac: add base service

---
+++ src/om.servicing.casemanagement.application/Services/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using OM.RequestFramework.Core.Logging;
+
+namespace om.servicing.casemanagement.application.Services;
+
+public abstract class BaseService
+{
+    private readonly ILoggingService _loggingService;
+
+    public BaseService(ILoggingService loggingService)
+    {
+        _loggingService = loggingService ?? throw new ArgumentNullException(nameof(loggingService));
+    }
+}
