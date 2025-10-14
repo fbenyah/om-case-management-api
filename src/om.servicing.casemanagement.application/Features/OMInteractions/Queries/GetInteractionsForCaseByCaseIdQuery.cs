6c54787d675fa2439ed92100650230af73e4c3cb
Francis Benyah <francis.benyah@gmail.com>
2025-10-14 16:47:27 +0200

    refac: initial queries for case and customer interactions

---
+++ src/om.servicing.casemanagement.application/Features/OMInteractions/Queries/GetInteractionsForCaseByCaseIdQuery.cs
@@ -0,0 +1,73 @@
+﻿using MediatR;
+using om.servicing.casemanagement.domain.Responses.Shared;
+using OM.RequestFramework.Core.Exceptions;
+
+namespace om.servicing.casemanagement.application.Features.OMInteractions.Queries;
+
+/// <summary>
+/// Represents a query to retrieve interactions associated with a specific case, identified by its case ID.
+/// </summary>
+/// <remarks>This query is used to fetch interaction details for a given case. The case is identified by the <see
+/// cref="CaseId"/> property. The response type for this query is <see
+/// cref="GetInteractionsForCaseByCaseIdResponse"/>.</remarks>
+public class GetInteractionsForCaseByCaseIdQuery : IRequest<GetInteractionsForCaseByCaseIdResponse>
+{
+    public string CaseId { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// Represents the response containing a list of interactions associated with a specific case ID.
+/// </summary>
+/// <remarks>This response is returned by operations that retrieve interactions for a given case.  The <see
+/// cref="Data"> property contains the list of interactions, represented as  <see cref="domain.Dtos.OMInteractionDto">
+/// objects. If no interactions are found, the list will be empty.</remarks>
+public class GetInteractionsForCaseByCaseIdResponse : ApplicationBaseResponse<List<domain.Dtos.OMInteractionDto>>, IResponse
+{
+    public GetInteractionsForCaseByCaseIdResponse()
+    {
+        Data = new List<domain.Dtos.OMInteractionDto>();
+    }
+}
+
+/// <summary>
+/// Handles queries to retrieve interactions associated with a specific case ID.
+/// </summary>
+/// <remarks>This class is responsible for processing <see cref="GetInteractionsForCaseByCaseIdQuery"/> requests
+/// and returning a <see cref="GetInteractionsForCaseByCaseIdResponse"/> containing the interactions related to the
+/// specified case ID. It uses the <see cref="Services.IOMInteractionService"/> to fetch the data.</remarks>
+public class GetInteractionsForCaseByCaseIdQueryHandler : SharedFeatures, IRequestHandler<GetInteractionsForCaseByCaseIdQuery, GetInteractionsForCaseByCaseIdResponse>
+{
+    private readonly Services.IOMInteractionService _interactionService;
+    public GetInteractionsForCaseByCaseIdQueryHandler
+        (
+            OM.RequestFramework.Core.Logging.ILoggingService loggingService,
+            Services.IOMInteractionService interactionService
+        )
+        : base(loggingService)
+    {
+        _interactionService = interactionService;
+    }
+
+    /// <summary>
+    /// Handles the retrieval of interactions associated with a specific case ID.
+    /// </summary>
+    /// <param name="request">The query containing the case ID for which interactions are to be retrieved.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="GetInteractionsForCaseByCaseIdResponse"/> containing the list of interactions for the specified
+    /// case ID. If the case ID is null, empty, or whitespace, the response will include an error message.</returns>
+    public async Task<GetInteractionsForCaseByCaseIdResponse> Handle(GetInteractionsForCaseByCaseIdQuery request, CancellationToken cancellationToken)
+    {
+        GetInteractionsForCaseByCaseIdResponse response = new();
+
+        if (string.IsNullOrWhiteSpace(request.CaseId))
+        {
+            response.SetOrUpdateErrorMessage("Case Id is required.");
+            return response;
+        }
+
+        List<domain.Dtos.OMInteractionDto> omInteractions = await _interactionService.GetInteractionsForCaseByCaseIdAsync(request.CaseId);
+        response.Data = omInteractions;
+
+        return response;
+    }
+}
---
+++ src/om.servicing.casemanagement.application/Features/OMInteractions/Queries/GetInteractionsForCaseByCustomerIdentificationQuery.cs
@@ -0,0 +1,78 @@
+﻿using MediatR;
+using om.servicing.casemanagement.domain.Responses.Shared;
+using OM.RequestFramework.Core.Exceptions;
+
+namespace om.servicing.casemanagement.application.Features.OMInteractions.Queries;
+
+/// <summary>
+/// Represents a query to retrieve interactions for a specific case based on the customer's identification number.
+/// </summary>
+/// <remarks>This query is used to fetch interactions associated with a case by providing the customer's unique
+/// identification number. The result of the query is returned as a <see
+/// cref="GetInteractionsForCaseByCustomerIdentificationResponse"/>.</remarks>
+public class GetInteractionsForCaseByCustomerIdentificationQuery : IRequest<GetInteractionsForCaseByCustomerIdentificationResponse>
+{
+      public string CustomerIdentificationNumber { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// Represents the response containing a list of interactions associated with a case,  retrieved based on customer
+/// identification.
+/// </summary>
+/// <remarks>This response includes a collection of interaction details encapsulated in  <see
+/// cref="domain.Dtos.OMInteractionDto"/> objects. The <see cref="Data"/> property  is initialized to an empty list by
+/// default.</remarks>
+public class GetInteractionsForCaseByCustomerIdentificationResponse : ApplicationBaseResponse<List<domain.Dtos.OMInteractionDto>>, IResponse
+{
+    public GetInteractionsForCaseByCustomerIdentificationResponse()
+    {
+        Data = new List<domain.Dtos.OMInteractionDto>();
+    }
+}
+
+/// <summary>
+/// Handles queries to retrieve interactions for a case based on a customer's identification number.
+/// </summary>
+/// <remarks>This handler processes the <see cref="GetInteractionsForCaseByCustomerIdentificationQuery"/> and
+/// returns a response containing a list of interactions associated with the specified customer identification number.
+/// If the customer identification number is null, empty, or whitespace, an error message is set in the
+/// response.</remarks>
+public class GetInteractionsForCaseByCustomerIdentificationQueryHandler : SharedFeatures, IRequestHandler<GetInteractionsForCaseByCustomerIdentificationQuery, GetInteractionsForCaseByCustomerIdentificationResponse>
+{
+    private readonly Services.IOMInteractionService _interactionService;
+    public GetInteractionsForCaseByCustomerIdentificationQueryHandler
+        (
+            OM.RequestFramework.Core.Logging.ILoggingService loggingService,
+            Services.IOMInteractionService interactionService
+        )
+        : base(loggingService)
+    {
+        _interactionService = interactionService;
+    }
+
+    /// <summary>
+    /// Handles the query to retrieve interactions for a case based on the provided customer identification number.
+    /// </summary>
+    /// <param name="request">The query containing the customer identification number used to retrieve interactions. The <see
+    /// cref="GetInteractionsForCaseByCustomerIdentificationQuery.CustomerIdentificationNumber"/> property must not be
+    /// null, empty, or whitespace.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="GetInteractionsForCaseByCustomerIdentificationResponse"/> containing the list of interactions
+    /// associated with the specified customer identification number. If the customer identification number is invalid,
+    /// the response will include an error message.</returns>
+    public async Task<GetInteractionsForCaseByCustomerIdentificationResponse> Handle(GetInteractionsForCaseByCustomerIdentificationQuery request, CancellationToken cancellationToken)
+    {
+        GetInteractionsForCaseByCustomerIdentificationResponse response = new();
+
+        if (string.IsNullOrWhiteSpace(request.CustomerIdentificationNumber))
+        {
+            response.SetOrUpdateErrorMessage("Customer Identification Number is required.");
+            return response;
+        }
+
+        List<domain.Dtos.OMInteractionDto> omInteractions = await _interactionService.GetInteractionsForCaseByCustomerIdentificationAsync(request.CustomerIdentificationNumber);
+        response.Data = omInteractions;
+
+        return response;
+    }
+}
