2d8d219a50be69eeed118b3554ad837687adc356
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 19:13:17 +0200

    refac: add create case command

---
+++ src/om.servicing.casemanagement.application/Features/OMCases/Commands/CreateOMCaseCommand.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using om.servicing.casemanagement.application.Services.Models;
+using om.servicing.casemanagement.domain.Dtos;
+using om.servicing.casemanagement.domain.Enums;
+using om.servicing.casemanagement.domain.Responses.Shared;
+using OM.RequestFramework.Core.Extensions;
+using OM.RequestFramework.Core.Logging;
+
+namespace om.servicing.casemanagement.application.Features.OMCases.Commands;
+
+public class CreateOMCaseCommand : IRequest<CreateOMCaseCommandResponse>
+{
+    public CaseChannel SourceChannel { get; set; } = CaseChannel.Unknown;
+    public string IdentificationNumber { get; set; } = string.Empty;
+}
+
+public class CreateOMCaseCommandResponse : ApplicationBaseResponse<BasicCaseCreateResponse>
+{
+    public CreateOMCaseCommandResponse()
+    {
+        Data = new BasicCaseCreateResponse();
+    }
+}
+
+public class CreateOMCaseCommandHandler : SharedFeatures, IRequestHandler<CreateOMCaseCommand, CreateOMCaseCommandResponse>
+{
+    private readonly Services.IOMCaseService _caseService;
+
+    public CreateOMCaseCommandHandler
+        (
+            ILoggingService loggingService,
+            Services.IOMCaseService caseService
+        )
+        : base(loggingService)
+    {
+        _caseService = caseService;
+    }
+
+    public async Task<CreateOMCaseCommandResponse> Handle(CreateOMCaseCommand request, CancellationToken cancellationToken)
+    {
+        var response = new CreateOMCaseCommandResponse();
+
+        OMCaseDto omCaseDto = new()
+        {
+            Channel = request.SourceChannel.GetDescription(),
+            IdentificationNumber = request.IdentificationNumber,
+            Status = CaseStatus.Initiated.GetDescription()
+        };
+
+        OMCaseCreateResponse createCaseserviceResponse = await _caseService.CreateCaseAsync(new OMCaseDto(), cancellationToken);
+        if (!createCaseserviceResponse.Success)
+        {
+            response.SetOrUpdateErrorMessage("Failed to create shell case.");
+
+            if (createCaseserviceResponse.ErrorMessages != null && createCaseserviceResponse.ErrorMessages.Any())
+            {
+                response.SetOrUpdateErrorMessages(createCaseserviceResponse.ErrorMessages);
+            }
+
+            if (createCaseserviceResponse.CustomExceptions != null && createCaseserviceResponse.CustomExceptions.Any())
+            {
+                response.SetOrUpdateCustomExceptions(createCaseserviceResponse.CustomExceptions);
+            }
+
+            return response;
+        }
+
+        response.Data = createCaseserviceResponse.Data;
+        return response;
+    }
+}
