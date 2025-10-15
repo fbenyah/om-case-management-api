6ecd57b581bdd6391b8394fb45a8283af8884ed9
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 08:44:54 +0200

    refac: add tests for query

---
+++ tests/om.servicing.casemanagement.tests/Application/Features/OMCases/Queries/GetCustomerCasesByIdentificationNumberAndStatusQueryTests.cs
@@ -0,0 +1,26 @@
+﻿using om.servicing.casemanagement.application.Features.OMCases.Queries;
+
+namespace om.servicing.casemanagement.tests.Application.Features.OMCases.Queries;
+
+public class GetCustomerCasesByIdentificationNumberAndStatusQueryTests
+{
+    [Fact]
+    public void Constructor_InitializesPropertiesToEmptyStrings()
+    {
+        var query = new GetCustomerCasesByIdentificationNumberAndStatusQuery();
+        Assert.Equal(string.Empty, query.Status);
+        Assert.Equal(string.Empty, query.IdentificationNumber);
+    }
+
+    [Fact]
+    public void Properties_CanBeSetAndRetrieved()
+    {
+        var query = new GetCustomerCasesByIdentificationNumberAndStatusQuery
+        {
+            Status = "Open",
+            IdentificationNumber = "123456"
+        };
+        Assert.Equal("Open", query.Status);
+        Assert.Equal("123456", query.IdentificationNumber);
+    }
+}
