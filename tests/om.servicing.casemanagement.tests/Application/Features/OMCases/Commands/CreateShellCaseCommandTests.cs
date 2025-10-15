223335b7ac8bc544708d2ce19f7474ea084e5772
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 19:11:06 +0200

    refac: add test for channel

--- tests/om.servicing.casemanagement.tests/Application/Features/OMCases/Commands/CreateShellCaseCommandTests.cs
+++ tests/om.servicing.casemanagement.tests/Application/Features/OMCases/Commands/CreateShellCaseCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using om.servicing.casemanagement.application.Features.OMCases.Commands;
+using om.servicing.casemanagement.domain.Enums;
 
 namespace om.servicing.casemanagement.tests.Application.Features.OMCases.Commands;
 
@@ -8,6 +9,17 @@ public class CreateShellCaseCommandTests
     public void Constructor_CanInstantiate()
     {
         var command = new CreateShellCaseCommand();
+        Assert.Equal(CaseChannel.Unknown, command.SourceChannel);
         Assert.NotNull(command);
+    }
+
+    [Fact]
+    public void Properties_CanBeSetAndRetrieved()
+    {
+        var command = new CreateShellCaseCommand
+        {
+            SourceChannel = CaseChannel.PublicWeb
+        };
+        Assert.Equal(CaseChannel.PublicWeb, command.SourceChannel);
     }
 }
