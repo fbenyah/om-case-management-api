79b5a17b3a7e0f8409a3a791d8a7a5ecf7712f53
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 19:10:28 +0200

    refac: tests for create OM case command

---
+++ tests/om.servicing.casemanagement.tests/Application/Features/OMCases/Commands/CreateOMCaseCommandResponseTests.cs
@@ -0,0 +1,64 @@
+﻿using om.servicing.casemanagement.application.Features.OMCases.Commands;
+using om.servicing.casemanagement.application.Services.Models;
+using OM.RequestFramework.Core.Exceptions;
+
+namespace om.servicing.casemanagement.tests.Application.Features.OMCases.Commands;
+
+public class CreateOMCaseCommandResponseTests
+{
+    [Fact]
+    public void Constructor_InitializesDataToBasicCaseCreateResponse()
+    {
+        var response = new CreateOMCaseCommandResponse();
+        Assert.NotNull(response.Data);
+        Assert.IsType<BasicCaseCreateResponse>(response.Data);
+        Assert.Empty(response.Data.Id);
+        Assert.Empty(response.Data.ReferenceNumber);
+    }
+
+    [Fact]
+    public void Data_CanBeSetAndRetrieved()
+    {
+        var basicResponse = new BasicCaseCreateResponse
+        {
+            Id = "CASE123",
+            ReferenceNumber = "REF456"
+        };
+        var response = new CreateOMCaseCommandResponse
+        {
+            Data = basicResponse
+        };
+        Assert.Equal("CASE123", response.Data.Id);
+        Assert.Equal("REF456", response.Data.ReferenceNumber);
+    }
+
+    [Fact]
+    public void SetOrUpdateErrorMessage_AddsErrorMessageAndSetsSuccessFalse()
+    {
+        var response = new CreateOMCaseCommandResponse();
+        response.SetOrUpdateErrorMessage("Some error occurred.");
+        Assert.Contains("Some error occurred.", response.ErrorMessages);
+        Assert.False(response.Success);
+    }
+
+    [Fact]
+    public void SetOrUpdateErrorMessages_AddsMultipleErrorMessages()
+    {
+        var response = new CreateOMCaseCommandResponse();
+        var errors = new List<string> { "Error 1", "Error 2" };
+        response.SetOrUpdateErrorMessages(errors);
+        Assert.Contains("Error 1", response.ErrorMessages);
+        Assert.Contains("Error 2", response.ErrorMessages);
+        Assert.False(response.Success);
+    }
+
+    [Fact]
+    public void SetOrUpdateCustomException_AddsCustomException()
+    {
+        var response = new CreateOMCaseCommandResponse();
+        var customException = new ClientException("Custom error");
+        response.SetOrUpdateCustomException(customException);
+        Assert.Contains(customException, response.CustomExceptions);
+        Assert.False(response.Success);
+    }
+}
---
+++ tests/om.servicing.casemanagement.tests/Application/Features/OMCases/Commands/CreateOMCaseCommandTests.cs
@@ -0,0 +1,28 @@
+﻿using om.servicing.casemanagement.application.Features.OMCases.Commands;
+using om.servicing.casemanagement.domain.Enums;
+
+namespace om.servicing.casemanagement.tests.Application.Features.OMCases.Commands;
+
+public class CreateOMCaseCommandTests
+{
+    [Fact]
+    public void Constructor_InitializesPropertiesToDefaults()
+    {
+        var command = new CreateOMCaseCommand();
+        Assert.NotNull(command);
+        Assert.Equal(CaseChannel.Unknown, command.SourceChannel);
+        Assert.Equal(string.Empty, command.IdentificationNumber);
+    }
+
+    [Fact]
+    public void Properties_CanBeSetAndRetrieved()
+    {
+        var command = new CreateOMCaseCommand
+        {
+            SourceChannel = CaseChannel.PublicWeb,
+            IdentificationNumber = "ID123"
+        };
+        Assert.Equal(CaseChannel.PublicWeb, command.SourceChannel);
+        Assert.Equal("ID123", command.IdentificationNumber);
+    }
+}
