d0b7a54694cf481840978b8f9810ec4346e57062
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 14:59:25 +0200

    refac: add create case response

---
+++ src/om.servicing.casemanagement.application/Services/Models/OMCaseCreateResponse.cs
@@ -0,0 +1,17 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMCaseCreateResponse : ServiceBaseResponse<BasicCaseCreateResponse>
+{
+    public OMCaseCreateResponse()
+    {
+        Data = new BasicCaseCreateResponse();
+    }
+}
+
+public class BasicCaseCreateResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string ReferenceNumber { get; set; } = string.Empty;
+}
