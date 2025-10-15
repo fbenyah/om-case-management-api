ae24a2b9b603b6f3463edfdc7a316c52e08a2f68
Francis Benyah <francis.benyah@gmail.com>
2025-10-15 12:40:46 +0200

    refac: class comments

--- src/om.servicing.casemanagement.domain/Responses/Shared/ServiceBaseResponse.cs
+++ src/om.servicing.casemanagement.domain/Responses/Shared/ServiceBaseResponse.cs
@@ -2,6 +2,13 @@
 
 namespace om.servicing.casemanagement.domain.Responses.Shared;
 
+/// <summary>
+/// Represents a base class for service responses, encapsulating the response data and metadata.
+/// </summary>
+/// <remarks>This class provides a common structure for service responses, including the response data and the
+/// time  the response was generated. It is intended to be used as a base class for more specific response
+/// types.</remarks>
+/// <typeparam name="T">The type of the data contained in the response.</typeparam>
 public abstract class ServiceBaseResponse<T> : BaseFluentValidationError
 {
     public DateTime ResponseTime { get; private set; } = DateTime.Now;
