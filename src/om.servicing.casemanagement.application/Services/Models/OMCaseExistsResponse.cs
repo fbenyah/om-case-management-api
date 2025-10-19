c8ad9ab9afd16f724e83087edc4fb0ecff4df5a3
Francis Benyah <francis.benyah@gmail.com>
2025-10-19 14:09:55 +0200

    refac: add a case exists response

---
+++ src/om.servicing.casemanagement.application/Services/Models/OMCaseExistsResponse.cs
@@ -0,0 +1,11 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMCaseExistsResponse : ServiceBaseResponse<bool>
+{
+    public OMCaseExistsResponse()
+    {
+        Data = false;
+    }
+}
