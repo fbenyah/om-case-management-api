7a1ce32d741e36d0a01b71a7b786458495f5c6b7
Francis Benyah <francis.benyah@gmail.com>
2025-10-21 19:36:33 +0200

    refac: add response models

---
+++ src/om.servicing.casemanagement.application/Services/Models/OMInteractionListResponse.cs
@@ -0,0 +1,11 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMInteractionListResponse : ServiceBaseResponse<List<domain.Dtos.OMInteractionDto>>
+{
+    public OMInteractionListResponse()
+    {
+        Data = new List<domain.Dtos.OMInteractionDto>();
+    }
+}
---
+++ src/om.servicing.casemanagement.application/Services/Models/OMTransactionListResponse.cs
@@ -0,0 +1,11 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMTransactionListResponse : ServiceBaseResponse<List<domain.Dtos.OMTransactionDto>>
+{
+    public OMTransactionListResponse()
+    {
+        Data = new List<domain.Dtos.OMTransactionDto>();
+    }
+}
