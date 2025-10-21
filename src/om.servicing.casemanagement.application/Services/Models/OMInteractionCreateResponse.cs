c686f1fa2556b211789a2779ce74febf1f87a144
Francis Benyah <francis.benyah@gmail.com>
2025-10-21 17:52:14 +0200

    refac: add create responses

---
+++ src/om.servicing.casemanagement.application/Services/Models/OMInteractionCreateResponse.cs
@@ -0,0 +1,19 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMInteractionCreateResponse : ServiceBaseResponse<BasicInteractionCreateResponse>
+{
+    public OMInteractionCreateResponse()
+    {
+        Data = new();
+    }
+}
+
+public class BasicInteractionCreateResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string ReferenceNumber { get; set; } = string.Empty;
+    public string CaseId { get; set; } = string.Empty;
+    public string CaseReferenceNumber { get; set; } = string.Empty;
+}
---
+++ src/om.servicing.casemanagement.application/Services/Models/OMTransactionCreateResponse.cs
@@ -0,0 +1,21 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+public class OMTransactionCreateResponse : ServiceBaseResponse<BasicTransactionCreateResponse>
+{
+    public OMTransactionCreateResponse()
+    {
+        Data = new();
+    }
+}
+
+public class BasicTransactionCreateResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string ReferenceNumber { get; set; } = string.Empty;
+    public string InteractionId { get; set; } = string.Empty;
+    public string InteractionReferenceNumber { get; set; } = string.Empty;
+    public string CaseId { get; set; } = string.Empty;
+    public string CaseReferenceNumber { get; set; } = string.Empty;
+}
