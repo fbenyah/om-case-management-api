5f820267bf46e69aed8454be8839f1f7d808173a
Francis Benyah <francis.benyah@gmail.com>
2025-10-21 07:31:14 +0200

    refac: write a base class for item exists

---
+++ src/om.servicing.casemanagement.application/Services/Models/BaseItemExistsResponse.cs
@@ -0,0 +1,16 @@
+﻿using om.servicing.casemanagement.domain.Responses.Shared;
+
+namespace om.servicing.casemanagement.application.Services.Models;
+
+/// <summary>
+/// Represents the base response for determining whether an item exists.
+/// </summary>
+/// <remarks>This class provides a boolean response indicating the existence of an item.  Derived classes can
+/// extend this functionality to include additional context or metadata.</remarks>
+public abstract class BaseItemExistsResponse : ServiceBaseResponse<bool>
+{
+    public BaseItemExistsResponse()
+    {
+        Data = false;
+    }
+}
