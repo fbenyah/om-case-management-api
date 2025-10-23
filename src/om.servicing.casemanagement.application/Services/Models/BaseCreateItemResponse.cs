704fe30e003450279b38978d94c10314a6ae0b35
Francis Benyah <francis.benyah@gmail.com>
2025-10-23 20:50:55 +0200

    refac: base create item class

---
+++ src/om.servicing.casemanagement.application/Services/Models/BaseCreateItemResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace om.servicing.casemanagement.application.Services.Models;
+
+public abstract class BaseCreateItemResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string ReferenceNumber { get; set; } = string.Empty;
+}
