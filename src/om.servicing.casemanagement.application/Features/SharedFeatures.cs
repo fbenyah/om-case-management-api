b5c332fcf50233797770bc82fb543823b2e5a7fc
Francis Benyah <francis.benyah@gmail.com>
2025-10-29 01:57:45 +0200

    refac: temporarily do not include navigational properties

--- src/om.servicing.casemanagement.application/Features/SharedFeatures.cs
+++ src/om.servicing.casemanagement.application/Features/SharedFeatures.cs
@@ -6,8 +6,8 @@ public abstract class SharedFeatures
 {
     protected internal readonly ILoggingService _loggingService;
 
-    protected readonly string[]? InteractionsTransactionsIncludeNavigationProperties = new[] { "Interactions", "Interactions.Transactions" };
-    protected readonly string[]? TransactionsIncludeNavigationProperties = new[] { "Transactions", };
+    protected readonly string[]? InteractionsTransactionsIncludeNavigationProperties = null; // new[] { "Interactions", "Interactions.Transactions" };
+    protected readonly string[]? TransactionsIncludeNavigationProperties = null; // new[] { "Transactions", };
 
     public SharedFeatures(
             ILoggingService loggingService
